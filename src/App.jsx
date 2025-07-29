dadcc0f8103e4303a18e36af0beba79a91e2d56a
joaopaulodecker <joaopaulodecker@gmail.com>
2025-07-29 19:56:58 -0300

    exclusão readme

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css"; // Pode manter este se quiser usar o arquivo para seus estil
 function App() {
   return (
     <div>
-      <h1>Minha Página em Branco</h1>
+      <h1>Minha Página em Branco haha </h1>
       <p>Agora posso começar a construir! hahahah</p>
     </div>
   );
