fe89517288bbe68f1585b41d38f9089e64aff98c
joaopaulodecker <joaopaulodecker@gmail.com>
2025-08-04 14:59:51 -0300

    excluindo a seção de depoimentos

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Benefits from "./components/Benefits/Benefits";
-import Testimonials from "./components/Testimonials/Tertimonials";
 import CTA from "./components/CTA/CTA";
 import Footer from "./components/Footer/Footer";
 
@@ -15,7 +14,6 @@ function App() {
         <Hero />
         <About />
         <Benefits />
-        <Testimonials />
         <CTA />
       </main>
       <Footer />
