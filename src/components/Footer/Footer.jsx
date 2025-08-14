dc5e0171c02444c1fb560e81b7db54e75abf9d74
joaopaulodecker <joaopaulodecker@gmail.com>
2025-08-14 15:56:22 -0300

    Atualização das fontes e do componente Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -19,46 +19,69 @@ function Footer() {
           <h3>Links Rápidos</h3>
           <ul>
             <li>
-              <a href="#cursos">Cursos</a>
+              <a
+                href="https://prograd.ufsc.br/cursos-de-graduacao-da-ufsc/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Cursos
+              </a>
             </li>
             <li>
-              <a href="#campus">Vida no Campus</a>
+              <a
+                href="https://ufsc.br/a-ufsc/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Vida no Campus
+              </a>
             </li>
             <li>
-              <a href="#ingresso">Como Ingressar</a>
+              <a
+                href="https://coperve.ufsc.br/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Como Ingressar
+              </a>
             </li>
             <li>
-              <a href="#">Site Oficial</a>
+              <a
+                href="https://ufsc.br/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Site Oficial
+              </a>
             </li>
           </ul>
         </div>
-
         <div className="footer-social">
           <h3>Siga-nos</h3>
           <div className="social-icons">
             <a
-              href="https://www.facebook.com/ufsc"
+              href="https://www.facebook.com/UniversidadeUFSC"
               target="_blank"
               rel="noopener noreferrer"
             >
               <FaFacebook />
             </a>
             <a
-              href="https://www.instagram.com/ufsc"
+              href="https://www.instagram.com/universidadeufsc"
               target="_blank"
               rel="noopener noreferrer"
             >
               <FaInstagram />
             </a>
             <a
-              href="https://www.youtube.com/ufsc"
+              href="https://www.youtube.com/@tvufsc"
               target="_blank"
               rel="noopener noreferrer"
             >
               <FaYoutube />
             </a>
             <a
-              href="https://www.twitter.com/ufsc"
+              href="https://x.com/ufsc"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -72,7 +95,10 @@ function Footer() {
           &copy; 2025 UFSC - Universidade Federal de Santa Catarina. Todos os
           direitos reservados.
         </p>
-        <p>Este é um projeto conceitual de landing page.</p>
+        <p>
+          Este é um projeto conceitual de landing page produzido por João Paulo
+          Decker Oleinik.
+        </p>
       </div>
     </footer>
   );
